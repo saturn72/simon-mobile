6486fac00b5e7a4a5ff932dcfcf369ff3f97de47
usl <uriyahshabtailevi123@gmail.com>
2022-08-24 11:05:26 +0300

    create mobile Application

---
+++ src/Simon/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Simon.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Simon.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ src/Simon/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Simon.Models;
+using Simon.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Simon.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
